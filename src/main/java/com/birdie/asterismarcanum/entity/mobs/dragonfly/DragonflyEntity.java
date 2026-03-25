60d72dd1335d0c165941b6aee0b59d72ac661ee6
BirdieVibes <jadjardavis@gmail.com>
2026-03-24 19:46:54 -0600

    dragonfly less stupid now, still in progress

--- src/main/java/com/birdie/asterismarcanum/entity/mobs/dragonfly/DragonflyEntity.java
+++ src/main/java/com/birdie/asterismarcanum/entity/mobs/dragonfly/DragonflyEntity.java
@@ -1,71 +1,84 @@
 package com.birdie.asterismarcanum.entity.mobs.dragonfly;
 
+import com.birdie.asterismarcanum.entity.mobs.lunar_moth.LunarMothEntity;
 import com.birdie.asterismarcanum.registries.ASAREntityRegistry;
 import io.redspace.ironsspellbooks.IronsSpellbooks;
 import net.acetheeldritchking.aces_spell_utils.registries.ASAttributeRegistry;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.NbtUtils;
+import net.minecraft.network.syncher.EntityDataAccessor;
+import net.minecraft.network.syncher.EntityDataSerializers;
+import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.tags.ItemTags;
+import net.minecraft.util.Mth;
+import net.minecraft.util.TimeUtil;
+import net.minecraft.util.valueproviders.UniformInt;
+import net.minecraft.world.Difficulty;
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.AgeableMob;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.Mob;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.control.FlyingMoveControl;
 import net.minecraft.world.entity.ai.control.LookControl;
 import net.minecraft.world.entity.ai.goal.*;
+import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
+import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
+import net.minecraft.world.entity.ai.goal.target.ResetUniversalAngerTargetGoal;
 import net.minecraft.world.entity.ai.navigation.FlyingPathNavigation;
 import net.minecraft.world.entity.ai.navigation.PathNavigation;
-import net.minecraft.world.entity.animal.Animal;
-import net.minecraft.world.entity.animal.FlyingAnimal;
+import net.minecraft.world.entity.ambient.AmbientCreature;
+import net.minecraft.world.entity.animal.*;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.pathfinder.PathType;
+import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib.animatable.GeoAnimatable;
 import software.bernie.geckolib.animatable.GeoEntity;
 import software.bernie.geckolib.animatable.instance.AnimatableInstanceCache;
 import software.bernie.geckolib.animation.*;
+import software.bernie.geckolib.animation.AnimationState;
 import software.bernie.geckolib.util.GeckoLibUtil;
 
+import java.util.Objects;
+import java.util.UUID;
+import java.util.function.Predicate;
 
-public class DragonflyEntity extends Animal implements GeoEntity, FlyingAnimal, GeoAnimatable {
+
+public class DragonflyEntity extends Animal implements GeoEntity, FlyingAnimal, GeoAnimatable, NeutralMob {
     private final AnimatableInstanceCache cache = GeckoLibUtil.createInstanceCache(this);
-    BlockPos anchorPoint;
 
     public DragonflyEntity(EntityType<? extends Animal> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
-        xpReward = 0;
+        xpReward = 5;
         this.lookControl = createLookControl();
-        this.moveControl = new FlyingMoveControl(this, 20, true);
-        this.anchorPoint = BlockPos.ZERO;
+        this.moveControl = new FlyingMoveControl(this, 40, true);
 
         this.setPathfindingMalus(PathType.DANGER_FIRE, -1.0F);
         this.setPathfindingMalus(PathType.WATER, -1.0F);
-        this.setPathfindingMalus(PathType.WATER_BORDER, 16.0F);
-        this.setPathfindingMalus(PathType.COCOA, -1.0F);
-        this.setPathfindingMalus(PathType.FENCE, -1.0F);
+        this.setPathfindingMalus(PathType.WATER_BORDER, -16.0F);
         noCulling = true;
     }
 
     public float getWalkTargetValue(BlockPos pos, LevelReader level) {
-        return level.getBlockState(pos).isAir() ? 10.0F : 0.0F;
+        return level.getBlockState(pos).isAir() ? 3.0F : 20.0F;
     }
 
     public DragonflyEntity(Level level, LivingEntity livingEntity) {
         this(ASAREntityRegistry.DRAGONFLY.get(), level);
     }
 
-    protected LookControl createLookControl()
-    {
+    protected LookControl createLookControl() {
         return new LookControl(this)
         {
             @Override
@@ -80,24 +93,112 @@ public class DragonflyEntity extends Animal implements GeoEntity, FlyingAnimal, 
         };
     }
 
+    private static final UniformInt PERSISTENT_ANGER_TIME;
+    private int remainingPersistentAngerTime;
+
+    @javax.annotation.Nullable
+    private UUID persistentAngerTarget;
+
     @Override
     public void registerGoals() {
         this.goalSelector.addGoal(0, new FloatGoal(this));
 
-        this.goalSelector.addGoal(1, new RestrictSunGoal(this));
-        this.goalSelector.addGoal(2, new WaterAvoidingRandomFlyingGoal(this, 0.5F));
+        this.goalSelector.addGoal(3, new WaterAvoidingRandomFlyingGoal(this, DragonflyEntity.this.getAttributeValue(Attributes.FLYING_SPEED)));
+        this.targetSelector.addGoal(1, (new HurtByTargetGoal(this, new Class[0])).setAlertOthers(new Class[0]));
+        this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(this, Player.class, 10, true, false, null));
 
+        this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, Bee.class, 10, true, false, null));
+        this.targetSelector.addGoal(1, new ResetUniversalAngerTargetGoal<>(this, true));
+        this.targetSelector.addGoal(1, new MoveTowardsTargetGoal(this, DragonflyEntity.this.getAttributeValue(Attributes.FLYING_SPEED), 24.0F));
         this.goalSelector.addGoal(3, new LookAtPlayerGoal(this, Player.class, 3.0F, 1.0F));
         this.goalSelector.addGoal(4, new LookAtPlayerGoal(this, Mob.class, 8.0F));
     }
+
+    public void travel(Vec3 travelVector) {
+        if (this.isControlledByLocalInstance()) {
+            if (this.isInWater()) {
+                this.moveRelative(0.02F, travelVector);
+                this.move(MoverType.SELF, this.getDeltaMovement());
+                this.setDeltaMovement(this.getDeltaMovement().scale((double)0.8F));
+            } else if (this.isInLava()) {
+                this.moveRelative(0.02F, travelVector);
+                this.move(MoverType.SELF, this.getDeltaMovement());
+                this.setDeltaMovement(this.getDeltaMovement().scale((double)0.5F));
+            } else {
+                this.moveRelative(this.getSpeed(), travelVector);
+                this.move(MoverType.SELF, this.getDeltaMovement());
+                this.setDeltaMovement(this.getDeltaMovement().scale((double)0.97F));
+            }
+        }
+
+        this.calculateEntityAnimation(false);
+    }
+
+    public void startPersistentAngerTimer() {
+        this.setRemainingPersistentAngerTime(PERSISTENT_ANGER_TIME.sample(this.random));
+    }
+
+    public void setRemainingPersistentAngerTime(int time) {
+        this.remainingPersistentAngerTime = time;
+    }
+
+    public int getRemainingPersistentAngerTime() {
+        return this.remainingPersistentAngerTime;
+    }
+
+    public void setPersistentAngerTarget(@javax.annotation.Nullable UUID target) {
+        this.persistentAngerTarget = target;
+    }
+
+    @javax.annotation.Nullable
+    public UUID getPersistentAngerTarget() {
+        return this.persistentAngerTarget;
+    }
+
+    public void tick() {
+        super.tick();
+//        if (this.level().isClientSide) {
+//            if (this.clientSideStandAnimation != this.clientSideStandAnimationO) {
+//                this.refreshDimensions();
+//            }
+//
+//            this.clientSideStandAnimationO = this.clientSideStandAnimation;
+//            if (this.isStanding()) {
+//                this.clientSideStandAnimation = Mth.clamp(this.clientSideStandAnimation + 1.0F, 0.0F, 6.0F);
+//            } else {
+//                this.clientSideStandAnimation = Mth.clamp(this.clientSideStandAnimation - 1.0F, 0.0F, 6.0F);
+//            }
+//        }
+//
+//        if (this.warningSoundTicks > 0) {
+//            --this.warningSoundTicks;
+//        }
+
+        if (!this.level().isClientSide) {
+            this.updatePersistentAnger((ServerLevel)this.level(), true);
+        }
+    }
+
+    static {
+        PERSISTENT_ANGER_TIME = TimeUtil.rangeOfSeconds(60, 80);
+    }
+
+//    public boolean isStanding() {
+//        return (Boolean)this.entityData.get(DATA_STANDING_ID);
+//    }
+//
+//    public void setStanding(boolean standing) {
+//        this.entityData.set(DATA_STANDING_ID, standing);
+//    }
 
     public static AttributeSupplier.Builder createAttributes() {
         return LivingEntity.createLivingAttributes()
                 .add(Attributes.MAX_HEALTH, 20.0)
                 .add(Attributes.FOLLOW_RANGE, 24.0)
-                .add(Attributes.MOVEMENT_SPEED, 0.5F)
-                .add(Attributes.ATTACK_DAMAGE, 3.0F)
-                .add(Attributes.FLYING_SPEED, 6.0F);
+                .add(Attributes.MOVEMENT_SPEED, 0.0F)
+                .add(Attributes.ATTACK_DAMAGE, 5.0F)
+                .add(Attributes.ENTITY_INTERACTION_RANGE, 4.0F)
+                .add(Attributes.FLYING_SPEED, 0.5F);
     }
 
     protected PathNavigation createNavigation(Level p_level) {
@@ -141,8 +242,7 @@ public class DragonflyEntity extends Animal implements GeoEntity, FlyingAnimal, 
         controllers.add(animationController);
     }
 
-    private PlayState predicate(AnimationState<DragonflyEntity> event)
-    {
+    private PlayState predicate(AnimationState<DragonflyEntity> event) {
         if (event.isMoving() && this.animationToPlay == null)
         {
             event.getController().setAnimation(RawAnimation.begin().then("idle_state", Animation.LoopType.LOOP));
@@ -178,6 +278,8 @@ public class DragonflyEntity extends Animal implements GeoEntity, FlyingAnimal, 
     // NBT
     @Override
     public void readAdditionalSaveData(CompoundTag pCompound) {
+        this.readPersistentAngerSaveData(this.level(), pCompound);
+
         super.readAdditionalSaveData(pCompound);
     }
 
@@ -193,6 +295,8 @@ public class DragonflyEntity extends Animal implements GeoEntity, FlyingAnimal, 
 
     @Override
     public void addAdditionalSaveData(CompoundTag pCompound) {
+        this.addPersistentAngerSaveData(pCompound);
+
         super.addAdditionalSaveData(pCompound);
     }
 
